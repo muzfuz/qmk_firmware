979400317b8945ecee747fced3fe06611f0b28e8
Mazin Power <mazinpower@notonthehighstreet.com>
2019-01-14 13:51:16 +0000

    Modifies planck key layout

--- keyboards/planck/keymaps/muzfuz/keymap.c
+++ keyboards/planck/keymaps/muzfuz/keymap.c
@@ -39,6 +39,10 @@ enum planck_keycodes
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 #define ESC_CTL CTL_T(KC_ESCAPE)
+#define LWR_KCF LT(_LOWER, KC_F)
+#define LWR_KCJ LT(_LOWER, KC_J)
+#define RSE_SCLN LT(_RAISE, KC_SCLN)
+#define CMD_ENT SGUI(KC_ENT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -55,9 +59,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_QWERTY] = LAYOUT_planck_grid(
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
-        ESC_CTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
+        ESC_CTL, KC_A, KC_S, KC_D, LWR_KCF, KC_G, KC_H, LWR_KCJ, KC_K, KC_L, RSE_SCLN, RGUI_T(KC_QUOT),
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-        BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER, KC_ENT, KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT),
+        CMD_ENT, KC_LCTL, KC_LALT, KC_LGUI, LOWER, KC_ENT, KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT),
 
     /* Lower
  * ,-----------------------------------------------------------------------------------.
@@ -72,8 +76,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
     [_LOWER] = LAYOUT_planck_grid(
-        KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
-        ESC_CTL, CLNEQLS, _______, KC_LCBR, KC_RCBR, KC_LPRN, KC_RPRN, KC_MINS, KC_EQL, KC_BSLS, KC_TILD, KC_RGUI,
+        KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+        ESC_CTL, CLNEQLS, _______, KC_LCBR, KC_RCBR, KC_LPRN, KC_RPRN, KC_MINS, KC_EQL, KC_BSLS, KC_TILD, RGUI_T(KC_GRV),
         _______, HASHRKT, _______, KC_LBRC, KC_RBRC, _______, _______, KC_UNDS, KC_PLUS, KC_PIPE, KC_HOME, KC_END,
         _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY),
 
@@ -89,10 +93,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
     [_RAISE] = LAYOUT_planck_grid(
-        KC_GRV,  _______, _______, KC_UP,   _______,  _______, _______, KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-        ESC_CTL, _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, KC_4,    KC_5,    KC_6,    _______, KC_RGUI,
-        _______, _______, _______, _______, _______,  _______, _______, KC_1,    KC_2,    KC_3,    _______, _______,
-        _______, _______, _______, _______, _______,  _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY),
+        KC_GRV, _______, _______, KC_UP, _______, _______, _______, KC_7, KC_8, KC_9, KC_0, KC_BSPC,
+        ESC_CTL, _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, KC_4, KC_5, KC_6, _______, KC_RGUI,
+        _______, _______, _______, _______, _______, _______, _______, KC_1, KC_2, KC_3, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY),
 
     /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
