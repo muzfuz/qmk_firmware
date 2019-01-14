d17c88a00b949a8ac3845312535bc2b651fef71e
Mazin Power <mazinpower@notonthehighstreet.com>
2019-01-14 15:38:14 +0000

    move layer triggers down again

--- keyboards/planck/keymaps/muzfuz/keymap.c
+++ keyboards/planck/keymaps/muzfuz/keymap.c
@@ -39,11 +39,8 @@ enum planck_keycodes
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 #define ESC_CTL CTL_T(KC_ESCAPE)
-#define LWR_KCF LT(_LOWER, KC_F)
-#define LWR_KCJ LT(_LOWER, KC_J)
-#define RSE_SCLN LT(_RAISE, KC_SCLN)
-#define RSE_KCA LT(_RAISE, KC_A)
-#define ADJ_BKSP LT(_ADJUST, KC_BSPC)
+#define LWR_SPC LT(_LOWER, KC_SPC)
+#define RSE_ENT LT(_RAISE, KC_ENT)
 #define CMD_ENT SGUI(KC_ENT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -60,10 +57,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
     [_QWERTY] = LAYOUT_planck_grid(
-        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, ADJ_BKSP,
-        ESC_CTL, KC_A, KC_S, KC_D, LWR_KCF, KC_G, KC_H, LWR_KCJ, KC_K, KC_L, RSE_SCLN, RGUI_T(KC_QUOT),
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
+        ESC_CTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, RGUI_T(KC_QUOT),
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-        CMD_ENT, KC_LCTL, KC_LALT, KC_LGUI, KC_NO, KC_ENT, KC_NO, KC_SPC, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT),
+        CMD_ENT, KC_LCTL, KC_LALT, KC_LGUI, KC_NO, RSE_ENT, KC_NO, LWR_SPC, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT),
 
     /* Lower
  * ,-----------------------------------------------------------------------------------.
@@ -112,7 +109,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
     [_ADJUST] = LAYOUT_planck_grid(
-        _______, RESET, DEBUG, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_DEL,
+        _______, RESET, DEBUG, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______,
         _______, _______, MU_MOD, AU_ON, AU_OFF, _______, _______, QWERTY, _______, _______, _______, _______,
         _______, MUV_DE, MUV_IN, MU_ON, MU_OFF, MI_ON, MI_OFF, TERM_ON, TERM_OFF, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______)
